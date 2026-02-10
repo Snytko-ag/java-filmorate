c746f104ee955ddfdebebb0cc36fd7da8254a698
Snytkoag <snytkoag25@yandex.ru>
2026-02-10 12:12:59 +0300

    Техническое задание проекта №12. Приложения Filmorate.

--- src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/like/LikeDbStorage.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
-import ru.yandex.practicum.filmorate.service.GenreService;
 import ru.yandex.practicum.filmorate.service.MpaService;
 import ru.yandex.practicum.filmorate.storage.mapper.FilmMapper;
 
